53f4f4cf05b416218b6f625611ecb9a796fcdf8b
Kevin Neibarger <kevinneibarger@macbook-pro-31.mynetworksettings.com>
2024-07-15 22:01:17 -0400

    Updated pages

--- src/pages/AlbumsList.js
+++ src/pages/AlbumsList.js
@@ -1,21 +1,20 @@
-// import {AlbumCard} from "../component";
-// import {useFetch} from "../hooks/useFetch";
-// import {useEffect} from "react";
-// import {useTitle} from "../hooks/useTitle";
-//
-// export const AlbumsList = ({apiPath, title}) => {
-//     const { data: movies } = useFetch(apiPath);
-//     useTitle(`${title} `);
-//
-//     return (
-//         <main>
-//             <section className="max-w-7xl mx-auto py-7">
-//                 <div className="flex justify-start flex-wrap other:justify-evenly">
-//                     { movies.map((movie) => (
-//                         <AlbumCard key={movie.id} movie={movie} />
-//                     )) }
-//                 </div>
-//             </section>
-//         </main>
-//     )
-// }
+import {AlbumCard} from "../component";
+import {useFetch} from "../hooks/useFetch";
+import {useTitle} from "../hooks/useTitle";
+
+export const AlbumsList = ({apiPath, title}) => {
+    const { data: movies } = useFetch(apiPath);
+    useTitle(`${title} `);
+
+    return (
+        <main>
+            <section className="max-w-7xl mx-auto py-7">
+                <div className="flex justify-start flex-wrap other:justify-evenly">
+                    { movies.map((movie) => (
+                        <AlbumCard key={movie.id} movie={movie} />
+                    )) }
+                </div>
+            </section>
+        </main>
+    )
+}
